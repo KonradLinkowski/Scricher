f754767963e50f7857d439e91429eb071eb07bc8
Konrad Linkowski <linkowski.konrad97@gmail.com>
2017-11-16 21:55:56 +0100

    Passwords hashing.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,12 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
 
 const user = require('../models/user');
 const keys = require('./keys')
 
 passport.use(new JwtStrategy({
-    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
     secretOrKey: keys.jwt.jwtKey
     }, (jwt_payload, done) => {
         user.getUser(jwt_payload.id, (err, user) => {
--- models/user.js
+++ models/user.js
@@ -1,20 +1,44 @@
-const db = require('../setup/database-setup.js');
+const db = require('../config/database-setup.js');
+const bcrypt = require('bcryptjs');
 
 const user = {};
 
 user.create = function(email, password, done) {
     let values = [email, password, new Date().toISOString()];
+    hashPassword(password, (err) => {
+        if (err) return done(err);
+    });
 
-    db.get().query('INSERT INTO users(email, password, date, date) VALUES(??, ??, ??, ??)', values, (err, result) => {
+    db.get().query('INSERT INTO users(email, password, creation, last_login) VALUES(??, ??, ??, ??)', values, (err, result) => {
         if (err) return done(err);
         done(null, result.insertID);
     });
 }
 
 user.getUser = function(userID, done) {
-    db.get().query('SELECT * FROM posts WHERE user_id = ??', userID, (err, rows) => {
+    db.get().query('SELECT userID FROM Users WHERE user_id = ??', userID, (err, rows) => {
         if (err) return done(err);
         done(null, rows);
+    });
+}
+
+function hashPassword(password, next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(password, salt, null, (err, hash) => {
+            if (err) return next(err);
+            password = hash;
+            next();
+        });
+    });
+}
+
+user.comparePassword = function (password, cb) {
+    bcrypt.compare(password, original, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
     });
 }
 
